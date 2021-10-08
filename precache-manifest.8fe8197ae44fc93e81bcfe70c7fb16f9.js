e83891d4f2b853582e2f909f757e1592c3cc05f5
Code Sandbox <hello@codesandbox.io>
2021-10-08 17:45:32 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8fe8197ae44fc93e81bcfe70c7fb16f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b9c9e61d249b76af899dcac55d117b",
+    "url": "https://dolly500.github.io/csb-st35l/index.html"
+  },
+  {
+    "revision": "300db525c3843d63f190",
+    "url": "https://dolly500.github.io/csb-st35l/static/css/2.58570433.chunk.css"
+  },
+  {
+    "revision": "bee1bcf1f0234acbedb7",
+    "url": "https://dolly500.github.io/csb-st35l/static/css/main.0d2bfa13.chunk.css"
+  },
+  {
+    "revision": "300db525c3843d63f190",
+    "url": "https://dolly500.github.io/csb-st35l/static/js/2.a0e55a2d.chunk.js"
+  },
+  {
+    "revision": "bee1bcf1f0234acbedb7",
+    "url": "https://dolly500.github.io/csb-st35l/static/js/main.a2ee18a9.chunk.js"
+  },
+  {
+    "revision": "4184a379f3b0349334cf",
+    "url": "https://dolly500.github.io/csb-st35l/static/js/runtime~main.e036dc3b.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "https://dolly500.github.io/csb-st35l/static/media/logo.9f7e506d.svg"
+  }
+]);
